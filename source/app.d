7a662f84c33bc3080bc7aa8f50b558211c92423a
Selim Ozel <sozel@wpi.edu>
2020-05-11 22:10:04 +0300

    Updated git ignore for dll and json selections. Updated dub sdl for vibe-d. Added a show hoem function under app.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,16 @@
 import std.stdio;
 
+import vibe.http.server;
+import vibe.http.fileserver;
+import vibe.http.router;
+import vibe.http.server;
+import std.functional : toDelegate;
+
+void showHome(HTTPServerRequest req, HTTPServerResponse res)
+{
+	res.redirect("/plain-react.html");
+}
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
