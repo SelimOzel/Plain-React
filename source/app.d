79d1627964039ae825d648ecce6f641597f76a17
Selim Ozel <sozel@wpi.edu>
2020-05-17 21:23:40 +0300

    Implemented a RESTful quote class. Very simple.

--- source/app.d
+++ source/app.d
@@ -4,6 +4,7 @@ import vibe.http.server;
 import vibe.http.fileserver;
 import vibe.http.router;
 import vibe.http.server;
+import vibe.web.rest;
 import std.functional : toDelegate;
 
 // To have console access
@@ -14,8 +15,24 @@ void showHome(HTTPServerRequest req, HTTPServerResponse res)
 	res.redirect("/plain-react.html");
 }
 
+// Defines a simple RESTful API
+interface IQuotes {
+	// GET /get_quote?index=..
+	@method(HTTPMethod.GET)
+	string[2] getQuote(int index);
+}
+
+// Local 1quote implementation
+class Quotes : IQuotes {
+	import std.stdio;
+	string[5] authors = ["Warren Buffett", "Mark Spitznagel", "Jim Simons", "Charlie Munger", "Miyamoto Musashi, Swordsman"];
+	string[5] quotes = ["Fear is the most contagious disease you can imagine.", "Everyone knows you can't fight the FED. What you must do is to Jiu Jitsu the FED.", "Past performance is the best predictor of success.", "It takes character to sit with all that cash and to do nothing. I didn't get top where I am by going after mediocre opportunities.", "To know ten thousand things, know one well."];	
+	string[2] getQuote(int index) { return [quotes[index], authors[index]]; }
+}
+
 shared static this()
 {
+
 	auto router = new URLRouter;
 	router.get("/", &showHome);
 	router.get("/Buffet", staticRedirect("/Buffet.html"));
